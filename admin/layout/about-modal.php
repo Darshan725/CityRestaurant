01f0b9487c4db6665cd359289d19d2ca7d7a2c94
Darshan725 <dkdarshan725@gmail.com>
2023-08-28 15:06:46 +0530

    web development college project

---
+++ admin/layout/about-modal.php
@@ -0,0 +1,9 @@
+<div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>About City Restaurant</h4>
+      <p>We are City Restaurant. One Restaurant powered by the boyas only! We love to make delicious foods and serve our customers. We have been doing this for many years now!</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>
+    </div>
+  </div>
